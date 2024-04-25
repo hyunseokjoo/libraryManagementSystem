53d5aab837e33e705047ef701765daf27ee60d5f
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2024-04-25 19:05:41 +0900

    Feat : book api

---
+++ src/LMS/book/models.py
@@ -0,0 +1,40 @@
+from typing import Optional, List
+
+from sqlalchemy import Column, Integer, String, Boolean
+
+from ..models import LMSBaseModel
+from ..database.core import Base
+
+
+
+
+class Book(Base):
+    __tablename__ = "book"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    borrowed = Column(Boolean)
+
+
+class BookBase(LMSBaseModel):
+    id : int
+    title : str
+    author : str
+    borrowed : bool
+    borrowing_userid : int
+
+class BookCreate(BookBase):
+    pass
+
+class BookRead(BookBase):
+    pass
+
+class BookUpdate(BookBase):
+    pass
+
+class BookDelete(BookBase):
+    pass
+
+class BookList(BookBase):
+    items : Optional[List[BookRead]] = []
+
