efba23611ccc44c234d2d644d2d321ca02738b48
Eddie <49854618+hyunseokjoo@users.noreply.github.com>
2024-04-25 19:02:16 +0900

    add : initial settings file

---
+++ initial.cmd
@@ -0,0 +1,15 @@
+@REM 가상환경 만들기 python -m venv <가상환경이름>
+python -m venv venv 
+
+@REM windows에서 실행
+cd venv/Scripts
+source ./venv/Scripts/activate
+
+@REM 필요한 의존 패키지 설치
+python.exe -m pip install --upgrade pip
+pip install -r ./requiremets.txt
+
+@REM webserver 실행
+uvicorn src.LMS.main:app --reload
+
+@REM 
